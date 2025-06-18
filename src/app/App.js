847f8dcd3a1c47eab81639e64adb07d9e0c8c277
DevLuxor08 <lucasmartines30@hotmail.com>
2025-06-18 20:29:10 -0300

    nn

--- src/app/App.js
+++ src/app/App.js
@@ -14,6 +14,7 @@ import Contato from "../pages/Contato/Contato";
 import Painel from "../pages/Painel"; 
 import Login from "../pages/Login"; 
 
+//Teste novo
 //AUTENTICAÇÂO
 import PrivateRoute from "../components/Auth";
 
